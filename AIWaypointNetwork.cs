6a37bdd69cd4ef0ccdb27f81d07a17800e27c67e
VandersaRichard <zf1045056938@163.com>
2018-10-31 11:52:05 +0800

    1111

---
+++ AIWaypointNetwork.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIWaypointNetwork : MonoBehaviour 
+{
+	public List<Transform> Waypoints = new List<Transform>();
+
+}
